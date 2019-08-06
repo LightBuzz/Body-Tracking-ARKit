4659506c4690a20628e716c8085b782b8993b5aa
konsnos <konsnosl@gmail.com>
2019-08-06 13:17:39 +0300

    Updated to arkit beta 5

--- body-tracking-arkit/Assets/Scripts/HumanBodyTracking.cs
+++ body-tracking-arkit/Assets/Scripts/HumanBodyTracking.cs
@@ -19,16 +19,18 @@ public class HumanBodyTracking : MonoBehaviour
     private LineRenderer[] lineRenderers;
     private Transform[][] lineRendererTransforms;
 
+    private const float jointScaleModifier = .4f;
+
     void OnEnable()
     {
-        Debug.Assert(humanBodyManager != null, "human body manager is required");
+        Debug.Assert(humanBodyManager != null, "Human body manager is required");
         humanBodyManager.humanBodiesChanged += OnHumanBodiesChanged;
     }
 
     void OnDisable()
     {
-        Debug.Assert(humanBodyManager != null, "human body manager is required");
-        humanBodyManager.humanBodiesChanged -= OnHumanBodiesChanged;
+        if (humanBodyManager != null)
+            humanBodyManager.humanBodiesChanged -= OnHumanBodiesChanged;
     }
 
     private void InitialiseObjects(Transform arBodyT)
@@ -37,20 +39,20 @@ public class HumanBodyTracking : MonoBehaviour
         {
             bodyJoints = new Dictionary<JointIndices3D, Transform>
             {
-                { JointIndices3D.head_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.neck_1_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_arm_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_arm_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_forearm_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_forearm_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_hand_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_hand_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_upLeg_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_upLeg_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_leg_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_leg_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.left_foot_joint, GetNewJointPrefab(arBodyT) },
-                { JointIndices3D.right_foot_joint, GetNewJointPrefab(arBodyT) }
+                { JointIndices3D.Head, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.Neck1, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftArm, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightArm, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftForearm, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightForearm, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftHand, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightHand, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftUpLeg, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightUpLeg, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftLeg, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightLeg, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.LeftFoot, GetNewJointPrefab(arBodyT) },
+                { JointIndices3D.RightFoot, GetNewJointPrefab(arBodyT) }
             };
 
             // Create line renderers
@@ -65,11 +67,11 @@ public class HumanBodyTracking : MonoBehaviour
 
             lineRendererTransforms = new Transform[][]
             {
-                new Transform[] { bodyJoints[JointIndices3D.head_joint], bodyJoints[JointIndices3D.neck_1_joint] },
-                new Transform[] { bodyJoints[JointIndices3D.right_hand_joint], bodyJoints[JointIndices3D.right_forearm_joint], bodyJoints[JointIndices3D.right_arm_joint], bodyJoints[JointIndices3D.left_arm_joint], bodyJoints[JointIndices3D.left_forearm_joint], bodyJoints[JointIndices3D.left_hand_joint]},
-                new Transform[] { bodyJoints[JointIndices3D.right_foot_joint], bodyJoints[JointIndices3D.right_leg_joint], bodyJoints[JointIndices3D.right_upLeg_joint], bodyJoints[JointIndices3D.left_upLeg_joint], bodyJoints[JointIndices3D.left_leg_joint], bodyJoints[JointIndices3D.left_foot_joint] },
-                new Transform[] { bodyJoints[JointIndices3D.right_arm_joint], bodyJoints[JointIndices3D.right_upLeg_joint] },
-                new Transform[] { bodyJoints[JointIndices3D.left_arm_joint], bodyJoints[JointIndices3D.left_upLeg_joint] }
+                new Transform[] { bodyJoints[JointIndices3D.Head], bodyJoints[JointIndices3D.Neck1] },
+                new Transform[] { bodyJoints[JointIndices3D.RightHand], bodyJoints[JointIndices3D.RightForearm], bodyJoints[JointIndices3D.RightArm], bodyJoints[JointIndices3D.Neck1], bodyJoints[JointIndices3D.LeftArm], bodyJoints[JointIndices3D.LeftForearm], bodyJoints[JointIndices3D.LeftHand]},
+                new Transform[] { bodyJoints[JointIndices3D.RightFoot], bodyJoints[JointIndices3D.RightLeg], bodyJoints[JointIndices3D.RightUpLeg], bodyJoints[JointIndices3D.LeftUpLeg], bodyJoints[JointIndices3D.LeftLeg], bodyJoints[JointIndices3D.LeftFoot] },
+                new Transform[] { bodyJoints[JointIndices3D.RightArm], bodyJoints[JointIndices3D.RightUpLeg] },
+                new Transform[] { bodyJoints[JointIndices3D.LeftArm], bodyJoints[JointIndices3D.LeftUpLeg] }
             };
 
             for (int i = 0; i < lineRenderers.Length; i++)
@@ -86,17 +88,11 @@ public class HumanBodyTracking : MonoBehaviour
 
     void UpdateBody(ARHumanBody arBody)
     {
-        if (jointPrefab == null)
-        {
-            Debug.Log("no prefab found");
-            return;
-        }
-
         Transform arBodyT = arBody.transform;
 
         if (arBodyT == null)
         {
-            Debug.Log("no root transform found for ARHumanBody");
+            Debug.Log("No root transform found for ARHumanBody");
             return;
         }
 
@@ -104,11 +100,15 @@ public class HumanBodyTracking : MonoBehaviour
 
         /// Update joint placement
         NativeArray<XRHumanBodyJoint> joints = arBody.joints;
+        if(!joints.IsCreated) return;
+
+        /// Update placement of all joints
         foreach (KeyValuePair<JointIndices3D, Transform> item in bodyJoints)
         {
             UpdateJointTransform(item.Value, joints[(int)item.Key]);
         }
 
+        /// Update all line renderers.
         for (int i = 0; i < lineRenderers.Length; i++)
         {
             lineRenderers[i].SetPositions(lineRendererTransforms[i]);
@@ -117,9 +117,9 @@ public class HumanBodyTracking : MonoBehaviour
 
     private void UpdateJointTransform(Transform jointT, XRHumanBodyJoint bodyJoint)
     {
-        jointT.localScale = bodyJoint.anchorScale;
-        jointT.localRotation = bodyJoint.anchorPose.rotation;
         jointT.localPosition = bodyJoint.anchorPose.position;
+        jointT.localRotation = bodyJoint.anchorPose.rotation;
+        jointT.localScale = bodyJoint.anchorScale * jointScaleModifier;
     }
 
     void OnHumanBodiesChanged(ARHumanBodiesChangedEventArgs eventArgs)
@@ -134,6 +134,6 @@ public class HumanBodyTracking : MonoBehaviour
             UpdateBody(humanBody);
         }
 
-        //Debug.Log("Bodies: " + (eventArgs.added.Count + eventArgs.updated.Count).ToString());
+        //Debug.Log($"Created {eventArgs.added.Count}, updated {eventArgs.updated.Count}, removed {eventArgs.removed.Count}");
     }
 }
